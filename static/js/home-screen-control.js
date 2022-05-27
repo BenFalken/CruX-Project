98c82f5897c872cbe253ee9ed275613f6d681518
Nathan Solomon <nathansolomon1678@gmail.com>
2022-05-27 15:06:56 -0700

    Replace images with canvas

--- static/js/home-screen-control.js
+++ static/js/home-screen-control.js
@@ -1,29 +1,43 @@
 $(document).ready(function(){
-    // Get current x position
-    let x = $("#avatar").css("left");
-    x = parseInt(x.slice(0, -2));
+    canvas = document.getElementById("game-canvas");
+    ctx = canvas.getContext("2d");
+    canvas.width = canvas.clientWidth;
+    // For some reason this was buggy when I tried just saying canvas.height = canvas.clientHeight
+    // But calculating the height from the width seems to work :)
+    canvas.height = canvas.clientWidth * 1080 / 1920;
 
-    // Initialize the socket.
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    // update game every 10 milliseconds
+    timestep = 20;
+    // keep track of current time (in milliseconds), current score, and highest score
+    time = 0;
+    score = 0;
+    highest_score = 0;
+    x = 100;
+    // direction of movement, either "left" or "right" (or neither???)
+    direction = "";
 
-    // This function makes sure our avatar does not exceed the bounds on the window, lengthwise
-    function clamp(val) {
-        var max = $(window).width()*(0.9 - 0.048);
-        var min = $(window).width()*(0.1 - 0.016);
-        return Math.min(Math.max(val, min), max);
+    // Initialize the socket and receive data from server
+    socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    socket.on('new_test_data', function(msg) {
+        direction = msg.direction_to_move;
+    });
+
+    terrain = new Image();
+    terrain.src = "/static/images/terrain.png";
+
+    puffy = [];
+    for (frame = 1; frame <= 12; frame++) {
+        puffy[frame - 1] = new Image();
+        puffy[frame - 1].src = "/static/images/smoothbrain/frame" + frame.toString() + ".png";
     }
 
-    // Receive details from server
-    socket.on('new_test_data', function(msg) {
-        // Update the direction of the avatar
-        if (msg.direction_to_move == 'left') {
-            x = clamp(x - $(window).width()*0.032);
-        }
-        else if (msg.direction_to_move == 'right') {
-            x = clamp(x + $(window).width()*0.32);
-        }
-        $("#avatar").css("left", x);
-    });
+    sparkle = []
+    for (frame = 1; frame <= 16; frame++) {
+        sparkle[frame - 1] = new Image();
+        sparkle[frame - 1].src = "/static/images/sparklejuice/frame" + frame.toString() + ".png";
+    }
+
+    window.requestAnimationFrame(update);
 });
 
 // Sets the play button to on and triggers the start_streaming function in app.py
@@ -43,4 +57,22 @@ function stop_playing() {
     $.getJSON('/stop_recording',
         function(data) {
     });
+}
+
+
+function update(timestamp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    window.requestAnimationFrame(update);
+
+    if (direction == "left") {
+        x -= 1;
+    } else if (direction == "right") {
+        x += 1;
+    }
+
+    ctx.drawImage(terrain, 0, 0, canvas.width, canvas.height);
+    // smoothbrain.gif has twelve frames that each last for 70 milliseconds
+    ctx.drawImage(puffy[Math.floor(timestamp / 70) % 12], x, canvas.height - canvas.width / 10, canvas.width / 10, canvas.width / 10);
+    // sparklejuice.gif has sixteen frames that each last for 70 milliseconds
+    ctx.drawImage(sparkle[Math.floor(timestamp / 70) % 16], 100, 100, canvas.width / 10, canvas.width / 10);
 }
